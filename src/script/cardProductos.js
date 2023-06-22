9e52be651a1739a097c48b5219e5e54b9534f275
samara633 <samara20031012@gmail.com>
2023-06-22 12:30:31 -0500

    cambio en cardproductos

--- src/script/cardProductos.js
+++ src/script/cardProductos.js
@@ -1,27 +1,16 @@
-<<<<<<< HEAD
-const contenedorProductos = document.getElementById('contenedor-productos');
-const carrito = [];
-function productosCarga(){
-    
-=======
 const contenedorProductos = document.querySelector("#contenedor-productos");
 
 function cargarProductos() {
 
     contenedorProductos.innerHTML = "";
 
->>>>>>> main
     productos.forEach(producto => {
         const div = document.createElement('div');
         div.classList.add("producto");
         div.innerHTML = `
             <img class="producto-imagen" src="${producto.imagen}" alt="${producto.alt}">
             <div class="producto-detalles">
-<<<<<<< HEAD
-                <p class="producto-precio1">${producto.precio}</p>  
-=======
                 <p class="producto-precio1">${producto.precio}</p>
->>>>>>> main
                 <h3 class="producto-titulo">${producto.nombre}</h3>
                 <p class="producto-precio">${producto.descripcion}</p>
                 <button class="producto-agregar" id="${producto.id}">Agregar</button>
@@ -30,11 +19,6 @@ function cargarProductos() {
         contenedorProductos.append(div);
     });
 
-<<<<<<< HEAD
-}
-productosCarga();
-=======
     actualizarBotonesAgregar();
 }
 cargarProductos();
->>>>>>> main
