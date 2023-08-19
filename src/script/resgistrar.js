315a42c114498898f3edf14b7a67cf91bf130f68
samara633 <samara20031012@gmail.com>
2023-08-18 19:42:23 -0500

    node.js for logueo and inicio de sesion

--- src/script/resgistrar.js
+++ src/script/resgistrar.js
@@ -1,23 +1,35 @@
-const signupForm = document.querySelector('#signupForm')
-signupForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const name = document.querySelector('#name').value
-    const email = document.querySelector('#email').value
-    const password = document.querySelector('#password').value
-    const Users = JSON.parse(localStorage.getItem('users')) || []
-    const isUserRegistered = Users.find(user => user.email === email)
-    if(isUserRegistered){
-        return alert('El usuario ya esta registrado! , intentalo con otro')
+const signupForm = document.querySelector('#signupForm');
+signupForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+
+    try {
+        const response = await fetch('/api/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ name, email, password })
+        });
+
+        const data = await response.json();
+        if (data.success) {
+            Swal.fire({
+                icon: 'Buen trabajo 👌!',
+                title:'🎉Registro exitoso!📋',
+                text: 'realicé el inicio de sesión por favor.',
+                showConfirmButton: false,
+                timer: 2000
+            }).then(() => {
+             window.location.href = '../pages/iniciarSesion.html'
+            })
+        } else {
+            alert('Error en el registro');
+        }
+    } catch (error) {
+        console.log('Error en el registro', error);
     }
-    Users.push({name: name, email: email, password: password})
-    localStorage.setItem('users', JSON.stringify(Users))
-    Swal.fire({
-       icon: 'Buen trabajo 👌!',
-       title:'🎉Registro exitoso!📋',
-       text: 'realicé el inicio de sesión por favor.',
-       showConfirmButton: false,
-       timer: 2000
-   }).then(() => {
-    window.location.href = '../pages/iniciarSesion.html'
-   })
-})
+});
+
